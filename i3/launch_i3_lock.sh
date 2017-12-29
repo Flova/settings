c2019a666c584f7a4c2022614e24c6646ef38e5a
Florian <florian@flova.de>
2017-12-29 18:11:51 +0100

    Adding differend Lockscreens

--- i3/launch_i3_lock.sh
+++ i3/launch_i3_lock.sh
@@ -4,7 +4,7 @@
 scrot /tmp/screen_locked.png
 
 #Edit it
-convert /tmp/screen_locked.png -fill black -colorize 50%  /tmp/screen_locked.png
+convert /tmp/screen_locked.png -fill black -colorize 40%  /tmp/screen_locked.png
 
 #launch i3lock
 i3lock -i /tmp/screen_locked.png
