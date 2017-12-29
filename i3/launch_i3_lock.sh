659c1aa5507da33257beb4e02d1b0a62b9e75cbc
Florian <florian@flova.de>
2017-12-29 17:27:40 +0100

    Adding Background Image for Lockscreen Script

---
+++ i3/launch_i3_lock.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+#take Screenshot
+scrot /tmp/screen_locked.png
+
+#Edit it
+convert /tmp/screen_locked.png -fill black -colorize 50%  /tmp/screen_locked.png
+
+#launch i3lock
+i3lock -i /tmp/screen_locked.png
