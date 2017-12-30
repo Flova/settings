a680a232a61758f3cbd5cc1f609ac9c705aa77db
Florian <florian@flova.de>
2017-12-30 01:24:38 +0100

    Modify the i3lock

--- i3/launch_i3_lock.sh
+++ i3/launch_i3_lock.sh
@@ -4,7 +4,12 @@
 scrot /tmp/screen_locked.png
 
 #Edit it
-convert /tmp/screen_locked.png -fill black -colorize 40%  /tmp/screen_locked.png
+#convert /tmp/screen_locked.png -fill black -colorize 40%  /tmp/screen_locked.png #Darken image
+
+#Edit it
+#convert /tmp/screen_locked.png -blur 3x3  /tmp/screen_locked.png #Blur Image
+
+convert /tmp/screen_locked.png -filter Gaussian -resize 25% -define filter:sigma=1 -resize 400% /tmp/screen_locked.png
 
 #launch i3lock
 i3lock -i /tmp/screen_locked.png
